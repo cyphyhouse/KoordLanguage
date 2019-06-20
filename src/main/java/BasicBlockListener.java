1524bbbf93114e6b8ad744a58cca068e6d6ee104
John Wang <jzw2@illinois.edu>
2019-06-20 17:09:18 -0500

    Added missing javadocs

--- src/main/java/BasicBlockListener.java
+++ src/main/java/BasicBlockListener.java
@@ -1,20 +1,55 @@
+/**
+ * Listener for traversing a control flow graph.
+ */
 public interface BasicBlockListener {
 
+    /**
+     * Called when the traversal enters a true block.
+     * @param block the block entered.
+     */
     void enterTrue(BasicBlock block);
 
+    /**
+     * Called when the traversal exits a true block.
+     * @param block the block exited.
+     */
     void exitTrue(BasicBlock block);
 
+    /**
+     * Called when the traversal enters a false block.
+     * @param block the block entered.
+     */
     void enterFalse(BasicBlock block);
 
+    /**
+     * Called when the traversal exits a false block.
+     * @param block the block exited.
+     */
     void exitFalse(BasicBlock block);
 
+    /**
+     * Called when the traversal enters a merge block.
+     * @param block the block entered.
+     */
     void enterSingle(BasicBlock block);
 
+    /**
+     * Called when the traversal exits a merge block.
+     * @param block the block exited.
+     */
     void exitSingle(BasicBlock block);
 
 
+    /**
+     * Called when the traversal enters any block.
+     * @param block the block entered.
+     */
     void enterNode(BasicBlock block);
 
+    /**
+     * Called when the traversal exits any block.
+     * @param block the block exited.
+     */
     void exitNode(BasicBlock block);
 
 
