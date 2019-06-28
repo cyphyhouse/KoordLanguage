d2e960b2fdc4ed88bd9f4078c5078516d9a1889b
John Wang <jzw2@illinois.edu>
2019-06-28 16:09:35 -0500

    Started mock tests for lineform

--- GeneratedPython/basicFollow.py
+++ GeneratedPython/basicFollow.py
@@ -12,20 +12,31 @@ def pos3d(a, b, c):
     return pos
 
 
+def midpoint(x, y):
+    mid_x = (x.position.x + y.position.x) / 2
+    mid_y = (x.position.y + y.position.y) / 2
+    mid_z = (x.position.z + y.position.z) / 2
+    ret = Pose()
+    ret.position.x, ret.position.y, ret.position.z = mid_x, mid_y, mid_z
+    return ret
+
 def log(message):
     print(str(message))
 
 
 class DefaultName(AgentThread):
 
-    def __init__(self, pid: int, num_bots: int):
-        super(DefaultName, self).__init__(Gvh(pid, num_bots), CommHandler("", 0, 0, pid))
+    def __init__(self, pid: int, num_bots: int, receiver_ip, r_port):
+        super(DefaultName, self).__init__(Gvh(pid, num_bots), CommHandler(receiver_ip, r_port))
         self.start()
 
     def write_to_shared(self, var_name, index, value):
-        pass
+        self.request_mutex(var_name)
+        self.agent_gvh.put(var_name, value, index)
 
     def read_from_shared(self, var_name, index):
+        self.agent_gvh.get(var_name, index)
+
         pass
 
     def read_from_sensor(self, var_name):
