6333d8a34fcf6c1404f1a6745b93cc3a86a08e58
John Wang <jzw2@illinois.edu>
2019-07-16 16:50:50 -0500

    Created codegen for task

---
+++ GeneratedPython/tasks.py
@@ -0,0 +1,34 @@
+from agentThread import AgentThread
+
+
+class DefaultName(AgentThread):
+
+    def __init__(self, config):
+        super(DefaultName, self).__init__(config)
+        self.start()
+
+    def initialize_vars(self):
+        self.locals = {}
+        self.locals['i'] = 0
+        self.locals['currentRoute'] = None
+        self.locals['newTask'] = None
+        self.locals['asgn'] = 0
+        self.locals['rchd'] = 1
+        self.locals['cmplt'] = 2
+        self.locals['stage'] = 0
+        self.create_aw_var('taskList', list, None)
+        self.create_ar_var('routes', list, None)
+
+    def loop_body(self):
+        if self.locals['stage'] == self.locals['asgn']:
+            self.locals['newTask'] = self.getAvailableNextTask(self.read_from_shared('taskList', None))
+            self.locals['currentRoute'] = self.getPathFromTask(self.locals['newTask'])
+            if not self.willCollide(self.locals['newTask']):
+                self.write_to_shared('routes', self.pid(), self.locals['currentRoute'])
+                self.write_to_actuator('Motion.route', self.locals['currentRoute'])
+                self.locals['stage'] = self.locals['rchd']
+
+            return
+        if self.locals['stage'] == self.locals['rchd'] and self.read_from_sensor('Motion.done'):
+            self.locals['stage'] = self.locals['asgn']
+            return
